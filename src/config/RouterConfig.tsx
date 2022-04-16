01d3ebaa534bf7dc1fc0baa14cd617e310d3c8d3
tohsaka888 <156132264@qq.com>
2022-04-16 21:49:29 +0800

    feat: 新增搜索页

--- src/config/RouterConfig.tsx
+++ src/config/RouterConfig.tsx
@@ -4,6 +4,7 @@ import useBreakPoint from "tohsaka888-use-breakpoint";
 import Header from "../components/Header";
 import { BreakpointContext } from "../Context/BreakpointContext";
 import HomePage from "../pages/HomePage";
+import Search from "../pages/Search/Index";
 
 function RouterConfig() {
   const { deferedSize, deferedWidth } = useBreakPoint();
@@ -15,6 +16,7 @@ function RouterConfig() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<HomePage />} />
+          <Route path="search/:name" element={<Search />} />
         </Routes>
       </BrowserRouter>
     </BreakpointContext.Provider>
