72c2aa50f0391092d53c9a381bc9c2e629d7ba70
tohsaka888 <156132264@qq.com>
2022-04-18 08:20:00 +0800

    feat: config router

--- src/config/RouterConfig.tsx
+++ src/config/RouterConfig.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import useBreakPoint from "tohsaka888-use-breakpoint";
 import Header from "../components/Header";
 import { BreakpointContext } from "../Context/BreakpointContext";
+import Animation from "../pages/Animation";
 import HomePage from "../pages/HomePage";
 import Search from "../pages/Search/Index";
 
@@ -17,6 +18,7 @@ function RouterConfig() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="search/:name" element={<Search />} />
+          <Route path="animation/:id" element={<Animation />} />
         </Routes>
       </BrowserRouter>
     </BreakpointContext.Provider>
